de2f4f01880b96b7dd8485c08973c48a6629edc5
Francisco Rocha <fa676533@gmail.com>
2026-07-31 01:49:07 -0600

    feat: header con logo y patrones de fondo con pseudoelementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 
 function App() {
 
-  return (
-    <div className="pointer-events-none absolute inset-0 bg-[url(./images/pattern-lines.svg)]
-        bg-center bg-cover bg-repeat"
-    >
-        <div className="pointer-events-none absolute inset-0 bg-[url(./images/pattern-circle.svg)]
-            bg-no-repeat bg-size-(--circle-width) md:bg-size-(--circle-width-m) -left-7 -top-11
-            md:-left-10 md:-top-20 lg:left-5"></div>
-        <div className="pointer-events-none absolute inset-0 bg-[url(./images/pattern-squiggly-line-top.svg)]
-            bg-no-repeat bg-size-(--squiggly-width) md:bg-size-(--squiggly-width-m) bg-top-right top-6 md:top"></div>
+    return (
+    <>
+        <div className="pointer-events-none absolute inset-0 bg-[url(/images/pattern-lines.svg)]
+            bg-center bg-cover bg-repeat
+            before:pointer-events-none before:absolute before:inset-0
+                before:bg-[url(/images/pattern-circle.svg)] before:bg-no-repeat before:bg-size-(--circle-width)
+                md:before:bg-size-(--circle-width-m) before:-left-7 before:-top-11 md:before:-left-10
+                md:before:-top-20 lg:before:left-5
+            after:pointer-events-none after:absolute after:inset-0
+                after:bg-[url(/images/pattern-squiggly-line-top.svg)]
+                after:bg-no-repeat after:bg-size-(--squiggly-width) md:after:bg-size-(--squiggly-width-m)
+                after:bg-top-right after:top-6"
+        ></div>
         <header>
-            <img src="./images/logo-full.svg" alt="logo" className="m-auto mt-8" />
+            <img src="/images/logo-full.svg" alt="Conference Ticket Generator" className="m-auto mt-8" />
         </header>
-      </div>
+    </>
   )
 }
 
